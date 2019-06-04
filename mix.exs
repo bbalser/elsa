aa82963ff6140760440a0a527b52d5e70842fd53
Brian Balser <bbalser@pillartechnology.com>
2019-06-04 13:16:51 -0400

    Updated mix and LICENSE

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Elsa.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
       test_paths: test_paths(Mix.env())
@@ -27,7 +29,8 @@ defmodule Elsa.MixProject do
       {:divo, "~> 1.1", only: [:dev, :test, :integration], override: true},
       {:divo_kafka, "~> 0.1.0", only: [:dev, :test, :integration]},
       {:placebo, "~> 1.2", only: [:dev, :test]},
-      {:checkov, "~> 0.4.0", only: [:test]}
+      {:checkov, "~> 0.4.0", only: [:test]},
+      {:ex_doc, "~> 0.20.2", only: [:dev]}
     ]
   end
 
@@ -36,4 +39,16 @@ defmodule Elsa.MixProject do
 
   defp test_paths(:integration), do: ["test/integration"]
   defp test_paths(_), do: ["test/unit"]
+
+  defp package do
+    [
+      maintainers: ["Brian Balser", "Jeff Grunewald"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/bbalser/elsa"}
+    ]
+  end
+
+  defp description do
+    "Elsa is a full-featured Kafka library written in Elixir and extending the :brod library with additional support from the :kafka_protocol Erlang libraries to provide capabilities not available in :brod."
+  end
 end
