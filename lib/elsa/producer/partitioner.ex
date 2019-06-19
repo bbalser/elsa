4f0f8cc56914240032ce309bd305f95c466c1cc2
jeffgrunewald <jeff@grunewalddesign.com>
2019-06-19 17:23:50 -0400

    Fixing unclosed typespec; bumping patch version

--- lib/elsa/producer/partitioner.ex
+++ lib/elsa/producer/partitioner.ex
@@ -8,7 +8,7 @@ defmodule Elsa.Producer.Partitioner do
   Randomly choose a topic partition uniformely from
   the available total number of partitions.
   """
-  @spec partition(:random, integer(), term() :: integer()
+  @spec partition(:random, integer(), term()) :: integer()
   def partition(:random, partition_count, _key) do
     :crypto.rand_uniform(0, partition_count)
   end
@@ -17,7 +17,7 @@ defmodule Elsa.Producer.Partitioner do
   Choose a topic partition based on an md5 hash of
   the supplied value, typically the message content.
   """
-  @spec partition(:md5, integer(), term() :: integer()
+  @spec partition(:md5, integer(), term()) :: integer()
   def partition(:md5, partition_count, key) do
     :crypto.hash(:md5, key)
     |> :binary.bin_to_list()
