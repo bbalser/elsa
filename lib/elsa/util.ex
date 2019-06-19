c40f4582c619f36f8760271dfe577c5db804798e
Brian Balser <bbalser@pillartechnology.com>
2019-06-19 14:46:03 -0400

    Added utility function to chunk collection by byte size

--- lib/elsa/util.ex
+++ lib/elsa/util.ex
@@ -1,6 +1,8 @@
 defmodule Elsa.Util do
   @moduledoc false
 
+  @default_max_chunk_size 900_000
+
   def with_connection(endpoints, type \\ :any, fun) when is_function(fun) do
     endpoints
     |> reformat_endpoints()
@@ -34,6 +36,11 @@ defmodule Elsa.Util do
     end
   end
 
+  def chunk_by_byte_size(collection, chunk_byte_size \\ @default_max_chunk_size, function \\ &get_byte_size/1) do
+    collection
+    |> Enum.chunk_while({0, []}, &chunk(&1, &2, chunk_byte_size, function), &after_chunk/1)
+  end
+
   defp connect(endpoints, :controller), do: :kpro.connect_controller(endpoints, [])
   defp connect(endpoints, _type), do: :kpro.connect_any(endpoints, [])
 
@@ -53,5 +60,39 @@ defmodule Elsa.Util do
       Exception.exception?(reason) -> Exception.format(:error, reason)
       true -> inspect(reason)
     end
+  end
+
+  defp chunk(item, {current_size, current_batch}, chunk_byte_size, function) do
+    item_size = function.(item)
+    new_total = current_size + item_size
+
+    case new_total < chunk_byte_size do
+      true -> add_item_to_batch(new_total, item, current_batch)
+      false -> finish_batch(item_size, item, current_batch)
+    end
+  end
+
+  defp add_item_to_batch(total, item, batch) do
+    {:cont, {total, [item | batch]}}
+  end
+
+  defp finish_batch(total, item, batch) do
+    {:cont, Enum.reverse(batch), {total, [item]}}
+  end
+
+  defp after_chunk({_size, []}) do
+    {:cont, {0, []}}
+  end
+
+  defp after_chunk({_size, current_batch}) do
+    finish_batch(0, nil, current_batch)
+  end
+
+  defp get_byte_size({one, two}) do
+    byte_size(one) + byte_size(two)
+  end
+
+  defp get_byte_size(item) do
+    byte_size(item)
   end
 end
