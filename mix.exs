36f64537e6fb4a174dee08b5dc1efa7a0ed97a5b
jeffgrunewald <jeff@grunewalddesign.com>
2019-07-10 00:26:00 -0400

    Bumping version for new feature consolidation

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Elsa.MixProject do
   def project do
     [
       app: :elsa,
-      version: "0.6.0",
+      version: "0.7.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
