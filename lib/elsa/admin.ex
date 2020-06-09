a54655b4b8aa7500fa4a736e7ca4dc31c8fdd4d0
jeffgrunewald <jeff@grunewalddesign.com>
2020-06-09 13:43:47 -0400

    wip: rustrdkafka admin integration

--- lib/elsa/admin.ex
+++ lib/elsa/admin.ex
@@ -1,3 +1,14 @@
 defmodule Elsa.Admin do
-  use Rustler, otp_app: :elsa_admin
+  use Rustler, otp_app: :elsa, crate: "elsa_admin"
+
+  def describe_topic(brokers, topic) do
+    brokers
+    |> Enum.map(fn {host, port} -> "#{to_string(host)}:#{to_string(port)}" end)
+    |> hd()
+    |> describe_topic_nif(topic)
+  end
+
+  defp describe_topic_nif(_brokers, _topic), do: :erlang.nif_error(:nif_not_loaded)
+
+  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/elsa_admin/src/lib.rs
+++ native/elsa_admin/src/lib.rs
@@ -1,7 +1,12 @@
-use rustler::{Encoder, Env, Error, Term};
+use futures::executor::block_on;
+use rdkafka::admin::{AdminClient, AdminOptions, ConfigResource, ResourceSpecifier};
+use rdkafka::client::DefaultClientContext;
+use rdkafka::error::RDKafkaError;
+use rdkafka::ClientConfig;
+use rustler::{Encoder, Env, Error, Term, types};
 
 mod atoms {
-    rustler_atoms! {
+    rustler::rustler_atoms! {
         atom ok;
         //atom error;
         //atom __true__ = "true";
@@ -12,9 +17,57 @@ mod atoms {
 rustler::rustler_export_nifs! {
     "Elixir.Elsa.Admin",
     [
-        ("describe_topic", 2, describe_topic)
+        ("add", 2, add),
+        ("describe_topic_nif", 2, describe_topic_nif)
     ],
     None
+}
+
+fn describe_topic_nif<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
+    let brokers: &str = args[0].decode()?;
+    let topic: &str = args[1].decode()?;
+
+    let result = block_on(describe_topic_inner(brokers, topic));
+    let config_hash = result[0]
+        .as_ref()
+        .ok()
+        .unwrap();
+    println!("{:?}", config_hash);
+    let config_map = types::map::map_new(env);
+
+    config_hash
+        .entries
+        .iter()
+        .map(|entry|
+             {
+                 let entry_map = types::map::map_new(env);
+                 entry_map.map_put("value".encode(env), entry.value.encode(env));
+                 config_map.map_put(entry.name.encode(env), entry_map.encode(env))
+             }
+        ).collect();
+
+    Ok((atoms::ok(), config_map).encode(env))
+}
+
+async fn describe_topic_inner(brokers: &str, topic: &str) -> Vec<Result<ConfigResource, RDKafkaError>> {
+    let admin_client = create_client(brokers);
+    let resource = [ResourceSpecifier::Topic(&topic)];
+    let opts = AdminOptions::new();
+
+    admin_client
+        .describe_configs(
+            &resource,
+            &opts,
+        )
+        .await
+        .expect("describe configs failed")
+}
+
+fn create_client(brokers: &str) -> AdminClient<DefaultClientContext> {
+    let mut client = ClientConfig::new();
+    client.set("bootstrap.servers", brokers)
+        .create()
+        .expect("admin client creation failed")
 }
 
 fn add<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
@@ -22,8 +75,4 @@ fn add<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
     let num2: i64 = args[1].decode()?;
 
     Ok((atoms::ok(), num1 + num2).encode(env))
-}
-
-fn describe_topic<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let brokers:
 }
