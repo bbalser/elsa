cd92430911060026bf38aa49125f0fd108b7097b
jeffgrunewald <jeff@grunewalddesign.com>
2020-06-25 12:55:18 -0400

    upgrading dependencies

--- mix.exs
+++ mix.exs
@@ -1,39 +1,43 @@
 defmodule Elsa.MixProject do
   use Mix.Project
 
+  @version "1.0.0-rc.2"
+  @github "https://github.com/bbalser/elsa"
+
   def project do
     [
       app: :elsa,
-      version: "1.0.0-rc.1",
+      name: "Elsa",
+      version: @version,
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
       deps: deps(),
+      homepage: @github,
+      docs: docs(),
       elixirc_paths: elixirc_paths(Mix.env()),
       test_paths: test_paths(Mix.env()),
-      dialyzer: [plt_file: {:no_warn, ".plt/dialyzer.plt"}]
+      dialyzer: [plt_file: {:no_warn, ".plt/#{System.version()}.plt"}]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:brod, "~> 3.9"},
+      {:brod, "~> 3.14"},
       {:patiently, "~> 0.2", only: [:dev, :test, :integration]},
       {:divo, "~> 1.1", only: [:dev, :test, :integration], override: true},
       {:divo_kafka, "~> 0.1.0", only: [:dev, :test, :integration]},
-      {:placebo, "~> 1.2", only: [:dev, :test]},
+      {:placebo, "~> 2.0.0-rc.2", only: [:dev, :test]},
       {:checkov, "~> 1.0", only: [:test, :integration]},
-      {:ex_doc, "~> 0.21.3", only: [:dev]},
-      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false}
+      {:ex_doc, "~> 0.22.1", only: [:dev]},
+      {:dialyxir, "~> 1.0.0", only: [:dev], runtime: false}
     ]
   end
 
@@ -47,11 +51,20 @@ defmodule Elsa.MixProject do
     [
       maintainers: ["Brian Balser", "Jeff Grunewald", "Johnson Denen"],
       licenses: ["Apache 2.0"],
-      links: %{"GitHub" => "https://github.com/bbalser/elsa"}
+      links: %{"GitHub" => @github}
     ]
   end
 
   defp description do
     "Elsa is a full-featured Kafka library written in Elixir and extending the :brod library with additional support from the :kafka_protocol Erlang libraries to provide capabilities not available in :brod."
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      source_url: @github,
+      extras: ["README.md"],
+      source_url_pattern: "#{@github}/blob/master/%{path}#L%{line}"
+    ]
   end
 end
